a73b9805201b26becb49c29a34c6b7adf2d12827
sora <133684349+sorawaslocked@users.noreply.github.com>
2025-05-27 00:38:52 +0500

    Add configuration

---
+++ internal/config/config.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"flag"
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/sorawaslocked/ap2final_base/pkg/grpc"
+	"github.com/sorawaslocked/ap2final_base/pkg/mongo"
+	"os"
+)
+
+type (
+	Config struct {
+		Env    string       `yaml:"env" env-required:"true"`
+		Mongo  mongo.Config `yaml:"mongo" env-required:"true"`
+		Server Server       `yaml:"server" env-required:"true"`
+	}
+
+	Server struct {
+		GRPC grpc.Config `yaml:"grpc" env-required:"true"`
+	}
+)
+
+func MustLoad() *Config {
+	cfgPath := fetchConfigPath()
+
+	if cfgPath == "" {
+		panic("config path is empty")
+	}
+
+	if _, err := os.Stat(cfgPath); os.IsNotExist(err) {
+		panic("config file does not exist: " + cfgPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(cfgPath, &cfg); err != nil {
+		panic("failed to load config")
+	}
+
+	return &cfg
+}
+
+func fetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "", "config file path")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
